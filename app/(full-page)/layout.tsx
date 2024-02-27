dca6ceb612a7cce3e47afe815d22927dd5a3d29f
Yeahya35 <yahyaeren35@hotmail.com>
2024-02-27 22:13:09 +0300

    repo push

---
+++ app/(full-page)/layout.tsx
@@ -0,0 +1,20 @@
+import { Metadata } from "next";
+import React from "react";
+import AppConfig from "../../layout/AppConfig";
+
+interface FullPageLayoutProps {
+    children: React.ReactNode;
+}
+
+export const metadata: Metadata = {
+    title: "Curnival",
+};
+
+export default function FullPageLayout({ children }: FullPageLayoutProps) {
+    return (
+        <React.Fragment>
+            {children}
+            <AppConfig minimal />
+        </React.Fragment>
+    );
+}
